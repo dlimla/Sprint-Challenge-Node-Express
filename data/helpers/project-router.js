0fd75cc0c5175cc431b5843d64d82656c359dc4b
Daniel Lim <danheelim@gmail.com>
2019-03-09 10:53:27 -0800

    adds a new model 'get' which gets all projects rather than single and adds post functionality

--- data/helpers/project-router.js
+++ data/helpers/project-router.js
@@ -16,6 +16,26 @@ router.get('/', async (req,res) => {
     }
 });
 
+
+router.get('/:id', async (req, res) => {
+    try {
+        const project = await Project.getById(req.params.id);
+        if(project) {
+            res.status(200).json(project);
+        }
+        else {
+            res.status(404).json({
+                message:"User not found"
+            })
+        }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: "ERROR in retrieving that project"
+        })
+    }
+})
+
 router.post('/', async (req,res) => {
     try {
         const project = await Project.insert(req.body);
